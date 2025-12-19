597e8e14a7e6a9544b09253587a0d428e83f8ed6
bala-amypo <bala@amypo.in>
2025-12-19 04:10:20 +0000

    User commit

--- src/main/java/com/example/demo/service/ConflictCaseService.java
+++ src/main/java/com/example/demo/service/ConflictCaseService.java
@@ -1,5 +1,5 @@
 import java.util.List;
-
+import com.example.demo.model.*;
 public interface ConflictCaseService {
     ConflictCase createCase(ConflictCase conflictCase);
     ConflictCase updateCaseStatus(Long caseId, String status);
